466e599cf288fcd7f77f16726270f9b0a2f4b89e
mattshardman <mattshardman@gmail.com>
2020-05-18 09:46:08 +0100

    longer

--- components/Component.tsx
+++ components/Component.tsx
@@ -10,7 +10,7 @@ const RightButton: React.FC<Props> = (props) => (
   <button {...props}>Right</button>
 );
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const Component: React.FC = () => (
   <div className={styles.component}>
