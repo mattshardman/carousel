6695c58b812220482d8b5f9ef4d201f45889f364
mattshardman <mattshardman@gmail.com>
2020-05-18 10:18:03 +0100

    change button styles

--- components/Component.tsx
+++ components/Component.tsx
@@ -5,16 +5,21 @@ import styles from "./component.module.css";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const LeftButton: React.FC<Props> = (props) => <button {...props}>Left</button>;
+const LeftButton: React.FC<Props> = (props) => <button style={{ width: 100 }} {...props}>Left</button>;
 const RightButton: React.FC<Props> = (props) => (
-  <button {...props}>Right</button>
+  <button style={{ width: 100 }} {...props}>Right</button>
 );
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const Component: React.FC = () => (
   <div className={styles.component}>
-    <Carousel LeftButton={LeftButton} RightButton={RightButton}>
+    <Carousel
+      LeftButton={LeftButton}
+      RightButton={RightButton}
+      speed={5}
+      infinite={false}
+    >
       {arr.map((each, i) => {
         return (
           <div className={styles.square} key={each}>
